bff57f38410524d9ad03b92fa6ea61f5ff95934c
Eric Mena <ericmenagatti@gmail.com>
2023-03-24 14:24:20 -0300

    chore: cleanup of pre-existing nextjs files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Goerli Survey</title>
+        <meta name="description" content="Gain QUIZ Token rewards by taking our surveys" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
