3c95f17659adba690c2ad31f81e4c2ab32d703ac
Eric Mena <ericmenagatti@gmail.com>
2023-03-25 12:19:00 -0300

    feat(metamask): Finished basic layout and web3 logic for Metamask

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
+import MainLayout from '@/components/layout/MainLayout';
+import EagerConnectGuard from '@/components/guard/EagerConnectGuard';
 import type { AppProps } from 'next/app'
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core'
+
+const getLibrary = (provider: any) => {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,7 +21,13 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <MainLayout>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <EagerConnectGuard>
+            <Component {...pageProps} />
+          </EagerConnectGuard>
+        </Web3ReactProvider>
+      </MainLayout>
     </>
   );
 };
